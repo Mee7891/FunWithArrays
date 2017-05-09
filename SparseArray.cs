af507426f043d858cc18290a042c7a2641159eb7
Mikhail Dubovik <m.dubovik@inbox.ru>
2017-05-09 20:51:19 +0500

    В класс SparseArray добавлен конструктор

--- SparseArray.cs
+++ SparseArray.cs
@@ -8,5 +8,22 @@ namespace MeeArraysClasses
 {
     public class SparseArray<T>
     {
+        int rowN;
+        int colN;
+
+        SortedList<int, SortedList<int, T>> rows;
+
+        public SparseArray(int _rowN, int _colN)
+        {
+            if (_rowN <= 0 || _colN <= 0) throw new Exception("Unacceptable array dimensions");
+
+            rowN = _rowN;
+            colN = _colN;
+
+            rows = new SortedList<int, SortedList<int, T>>(rowN);
+        }
+
+
+
     }
 }
